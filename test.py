b96b103c23490c7bcf6986d8b8f32c70ee33795a
MBogert <mabogert64@gmail.com>
2022-06-01 15:19:36 -0700

    Initial testfile

---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+
+# To Test
+# python -m unittest test.py
+# TODO
+# Fill with meaningful tests
+class FirstTest(unittest.TestCase):
+    def test_test(self):
+        self.assertTrue(True)
